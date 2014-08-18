c9fff26d4e4b7752cbb8c146adc0045c8a1d3753
James Christie <jwchristie.jc@gmail.com>
2014-08-18 18:27:45 +0100

    reconfig controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,4 @@
 class UsersController < ApplicationController
-  
-  def new
-      @user = User.new
-    end
-
-    def create
-      @user = User.new(params[:user])
-      if @user.save
-        redirect_to @user, :notice => "Successfully created user."
-      else
-        render :action => 'new'
-      end
-    end
 
   def show
     @user = User.find(current_user.id)
