d4b15ec9ec306dd960c9ec0004eddc8095eb0e4d
Smaton1 <sam_maton@hotmail.com>
2014-08-18 18:35:06 +0100

    created the associations between the users model and the user_connections model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :dob, :image_1, :image_2, :image_3, :gender, :looking_for, :profile_pic, :about_me, :post_code
   # attr_accessible :title, :body
+  has_many(:user_connections, :foreign_key => :user_1_id, :dependent => :destroy)
+  has_many(:reverse_user_connections, :class_name => :UserConnection, :foreign_key => :user_2_id, :dependent => :destroy)
+  has_many :users, :through => :user_connections, :source => :user_2
+
 end
+
