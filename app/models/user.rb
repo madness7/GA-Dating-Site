4bfd41ace6012b2e73af62dca0e6c953eec95226
Smaton1 <sam_maton@hotmail.com>
2014-08-19 14:13:40 +0100

    added in the first and last name fields from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,7 +57,10 @@ class User < ActiveRecord::Base
         where(auth.slice(:provider, :uid)).first_or_create do |user|
             user.provider = auth.provider
             user.uid = auth.uid
+            user.first_name = auth.info.first_name
+            user.last_name = auth.info.last_name
             user.email = auth.info.email
+            user.gender = auth.info.gender
             user.image_1 = auth.info.image
             user.password = Devise.friendly_token[0,20]
             # user.facebook_token = auth.credentials.token
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ DatingApp::Application.routes.draw do
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks" }
 
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 root to: "users#show"
