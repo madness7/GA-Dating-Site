27ffb341baa8e5f1b4e453c293df52d36192444b
Graham Curl <gac.curl@gmail.com>
2014-08-19 09:14:14 +0100

    carrier_wave implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :dob, :image_1, :image_2, :image_3, :gender, :looking_for, :profile_pic, :about_me, :post_code
   # attr_accessible :title, :body
+
+  mount_uploader :image_1, Image1Uploader
+  mount_uploader :image_2, Image2Uploader
+  mount_uploader :image_3, Image3Uploader
 end
