940ca506288a486e312dd3380fd31fae6db0ce4b
Graham Curl <gac.curl@gmail.com>
2014-08-20 21:41:44 +0100

    added method to user model to limit the fields searchable through ransack advanced search. This is achieved using an UNTRACKABLE_ATTRIBUTES array to list those columns that are not part of the drop down list

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,20 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :interests
 
 
-   has_many(:chats, :foreign_key => :user_1_id, :dependent => :destroy)
+  has_many(:chats, :foreign_key => :user_1_id, :dependent => :destroy)
   has_many(:reverse_chats, :class_name => :Chat, :foreign_key => :user_2_id, :dependent => :destroy)
   has_many :users, :through => :chats, :source => :user_2
-
   has_many :posts, :through => :post_connections, :source => :post_b
-
 
   mount_uploader :image_1, Image1Uploader
   mount_uploader :image_2, Image2Uploader
   mount_uploader :image_3, Image3Uploader
+
+  UNRANSACKABLE_ATTRIBUTES = ["reset_password_token", "reset_password_sent_at", "remember_created_at", "created_at", "dob", "image_1", "image_2", "image_3", "profile_pic", "uid", "provider", "updated_at", "unconfirmed_email", "confirmation_sent_at", "confirmed_at", "confirmation_token", "last_sign_in_ip", "current_sign_in_ip", "last_sign_in_at",  "current_sign_in_at", "sign_in_count", "encrypted_password", "user_id", "id", "about_me"  ]
+
+  def self.ransackable_attributes auth_object = nil
+      (column_names - UNRANSACKABLE_ATTRIBUTES) + _ransackers.keys
+  end
   
   def self.from_omniauth(auth)
     
