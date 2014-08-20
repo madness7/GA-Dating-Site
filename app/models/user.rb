1dfe332fa0023ab22ff76d75db340392af1c0b67
Graham Curl <gac.curl@gmail.com>
2014-08-20 23:46:00 +0100

    added CONSTANT = hash for ransack predicates to limit the list available to users for search

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+
+  RANSACK_PREDICATES = {"starts with" => :start, "contains" => :cont}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,  :omniauthable, omniauth_providers: [:facebook]
+  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :dob, :image_1, :image_2, :image_3, :gender, :looking_for, :profile_pic, :about_me, :post_code, :provider, :uid, :interest_ids
   # attr_accessible :title, :body
