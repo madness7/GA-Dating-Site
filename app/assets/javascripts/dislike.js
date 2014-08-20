2444d872bd74ec0183af7b0007ee511a48a733fc
James Christie <jwchristie.jc@gmail.com>
2014-08-20 10:45:27 +0100

    working like button

--- app/assets/javascripts/dislike.js
+++ app/assets/javascripts/dislike.js
@@ -1,5 +1,4 @@
 var railsToDo = railsToDo || {};
-console.log("hello")
 
 railsToDo.fields = ["user_1_id", "user_2_id", "negative_connection"]
 var id = $('#relationship').val()
@@ -12,19 +11,26 @@ railsToDo.postNote = function(event){
     var method = "POST";
     var path = "/user_connections";
     var data = {user_connections: {'user_1_id': parseInt($('#current_user').val()), 'user_2_id': parseInt($('#this_user').val()), 'negative_connection': true }} 
+
   }else{
-    
     var method = "PUT";
     var path = "/user_connections/"+id;
-    var data = {user_connections: {'user_1_id': parseInt($('#current_user').val()), 'user_2_id': parseInt($('#this_user').val()), 'negative_connection': !($("#hate").val()) }} 
-    
+    if($("#hate").val()=="true"){
+      var bool = false
+    }else if($("#hate").val()=="false"){
+      var bool = true
+    }
+    var data = {user_connections: {'user_1_id': parseInt($('#current_user').val()), 'user_2_id': parseInt($('#this_user').val()), 'negative_connection': bool }} 
   }
     $.ajax({
       url: path,
       method: method,
       data: data,
       dataType: "json"
-    }).success(function(){
+    }).success(function(data){
+      if(data){
+      $('#relationship').val(data.id)
+    }
       railsToDo.getNotes()
     })
   }
@@ -37,18 +43,12 @@ railsToDo.getNotes = function(){
       method: "GET",
       dataType: "json"
     }).success(function(data){
-      
-      if(data.negative_connection){
-        $("#hate").toggleClass("hated")
-        if($("#hate").val(true)){
-          $("#hate").val(false)
-        }else{
-          $("#hate").val(true)
-        }
-        
-      }else{
-        $("#hate").removeClass("hated")
-        $("#hate").val(false)
+      if(data.negative_connection==true){
+        $("#hate").addClass("hated")
+        $("#hate").val(true)
+      }else if(data.negative_connection==false){
+       $("#hate").removeClass("hated")
+       $("#hate").val(false) 
       }
     })
   }
