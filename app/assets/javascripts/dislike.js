029151eea396396922e331299023670a3d2f8cd5
James Christie <jwchristie.jc@gmail.com>
2014-08-20 22:49:36 +0100

    working like and dislike button interacting

--- app/assets/javascripts/dislike.js
+++ app/assets/javascripts/dislike.js
@@ -4,6 +4,36 @@ railsToDo.fields = ["user_1_id", "user_2_id", "negative_connection"]
 var id = $('#relationship').val()
 
 
+railsToDo.likeNote = function(event){
+  event.preventDefault();
+  var id = $('#relationship').val()
+  if(id==""){
+    var method = "POST";
+    var path = "/user_connections";
+    var data = {user_connections: {'user_1_id': parseInt($('#current_user').val()), 'user_2_id': parseInt($('#this_user').val()), 'negative_connection': false, 'interacted': true }}
+  }else{
+    var method = "PUT";
+    var path = "/user_connections/"+id;
+    if($("#like").val()=="true"){
+      var bool = false
+    }else if($("#like").val()=="false"){
+      var bool = true
+    }
+    var data = {user_connections: {'user_1_id': parseInt($('#current_user').val()), 'user_2_id': parseInt($('#this_user').val()), 'negative_connection': false, 'interacted': bool }}
+  }
+  $.ajax({
+    url: path,
+    method: method,
+    data: data,
+    dataType: "json"
+  }).success(function(data){  
+    if(data){
+      $('#relationship').val(data.id)
+    }
+    railsToDo.getNotes()
+  })
+}
+
 railsToDo.postNote = function(event){
   event.preventDefault();
   var id = $('#relationship').val()
@@ -18,6 +48,9 @@ railsToDo.postNote = function(event){
     if($("#hate").val()=="true"){
       var bool = false
     }else if($("#hate").val()=="false"){
+      var bool = true
+    }
+    if(bool==undefined){
       var bool = true
     }
     var data = {user_connections: {'user_1_id': parseInt($('#current_user').val()), 'user_2_id': parseInt($('#this_user').val()), 'negative_connection': bool }} 
@@ -43,14 +76,26 @@ railsToDo.getNotes = function(){
       method: "GET",
       dataType: "json",
       success: function(data){
-        if(data.negative_connection==true){
-          $("#hate").addClass("hated")
-          $("#hate").val(true)
-          $("#chat").addClass("hidden")
-        }else if(data.negative_connection==false){
-         $("#hate").removeClass("hated")
-         $("#hate").val(false) 
-         $("#chat").removeClass("hidden")
+        if(data.interacted==true){
+          $("#like").addClass("liked")
+          $("#like").val(true)
+          $("#hate").addClass("hidden")
+          $("#chat").removeClass("hidden")
+          return
+        }else if(data.interacted==false){
+          $("#like").removeClass("liked")
+          $("#like").val(false)
+          $("#hate").removeClass("hidden")
+          $("#chat").removeClass("hidden")
+          if(data.negative_connection==true){
+            $("#hate").addClass("hated")
+            $("#hate").val(true)
+            $("#chat").addClass("hidden")
+          }else if(data.negative_connection==false){
+            $("#hate").removeClass("hated")
+            $("#hate").val(false) 
+            $("#chat").removeClass("hidden")
+          }
         }
       }
     })
@@ -59,5 +104,6 @@ railsToDo.getNotes = function(){
 
 $(function(){
   railsToDo.getNotes();
+  $("#like").on("click", railsToDo.likeNote);
   $("#hate").on("click", railsToDo.postNote)
 });
