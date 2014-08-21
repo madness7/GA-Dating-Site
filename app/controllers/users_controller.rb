7ddec045da8c5965c1c5a6542cd923ac0616e6b9
Graham Curl <gac.curl@gmail.com>
2014-08-21 16:33:59 +0100

    fs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,7 @@ class UsersController < ApplicationController
       puts 'I am inside the if statment'
       @user = User.find(params[:id])
     else
-      puts '-' * 90 
-      puts current_user
-      puts current_user.id
-      users1 = User.where("id != ?", current_user.id)
-      puts '1' * 90 
-      puts users1
-      users2 = users1.looking(current_user.looking_for, current_user.gender)
-      puts '2' * 90 
-      puts users2
-      users3 = users2.interests(users2, current_user.interests[0].name)
-      puts '3' * 90 
-      puts users3
-      @user = users3.shuffle.first
-      puts '4' * 90 
-      puts @user
-      puts @user.id
+      @user = User.first
     end
     
     @connections = current_user.user_connections
