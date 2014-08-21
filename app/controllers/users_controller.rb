4c5ac7063503a27e4c0370920e6f63e22e0e7ee2
Graham Curl <gac.curl@gmail.com>
2014-08-21 16:28:21 +0100

    Changed instance variables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,17 +14,17 @@ class UsersController < ApplicationController
       puts '-' * 90 
       puts current_user
       puts current_user.id
-      @users = User.where("id != ?", current_user.id)
+      users1 = User.where("id != ?", current_user.id)
       puts '1' * 90 
-      puts @users
-      @users = @users.looking(current_user.looking_for, current_user.gender)
+      puts users1
+      users2 = users1.looking(current_user.looking_for, current_user.gender)
       puts '2' * 90 
-      puts @users
-      @users = @users.interests(@users, current_user.interests[0].name)
+      puts users2
+      users3 = users2.interests(@users, current_user.interests[0].name)
       puts '3' * 90 
-      puts @users
-      @user = @users.shuffle.first
-      puts 'p' * 90 
+      puts users3
+      @user = users3.shuffle.first
+      puts '4' * 90 
       puts @user
       puts @user.id
     end
